2f4111c284d775901fa886e8ff7f7773bfa21d14
AmandaLimaLuiz <delimaluiz.amanda@gmail.com>
2023-03-19 13:37:47 -0300

    icons

--- src/app/courses/services/curses.service.ts
+++ src/app/courses/services/curses.service.ts
@@ -15,7 +15,7 @@ export class CursesService {
   list(){
     return this.httpClient.get<Course[]>(this.API).pipe(
       first(),
-      delay(2000),
+      //delay(2000),
       tap(courses => console.log(courses))
     );
   }
---
+++ src/app/shared/pipes/category.pipe.ts
@@ -0,0 +1,17 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'category'
+})
+export class CategoryPipe implements PipeTransform {
+
+  transform(value: string): string {
+    switch(value){
+      case 'front-end': return 'code'
+      case 'back-end': return 'data_object'
+    }
+    return 'computer'
+
+  }
+
+}
