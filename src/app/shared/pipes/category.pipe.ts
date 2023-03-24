93c1b5b4d997d64767da912b9730966df0704520
AmandaLimaLuiz <delimaluiz.amanda@gmail.com>
2023-03-24 15:29:16 -0300

    Deploy

--- src/app/shared/pipes/category.pipe.ts
+++ src/app/shared/pipes/category.pipe.ts
@@ -7,8 +7,8 @@ export class CategoryPipe implements PipeTransform {
 
   transform(value: string): string {
     switch(value){
-      case 'front-end': return 'code'
-      case 'back-end': return 'data_object'
+      case 'Front-end': return 'code'
+      case 'Back-end': return 'data_object'
     }
     return 'computer'
 
